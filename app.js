d2580ac6b981e798aea31d3474c09e7eaf789974
Mikhail Semin <bifot@bifot.ru>
2016-12-24 01:22:37 +0300

    Added wall posts, names of shitty publics, some new styles and async parsing.

--- app.js
+++ app.js
@@ -1,121 +1,157 @@
 var express = require("express");
+var rp = require("request-promise");
+var cheerio = require("cheerio");
 var request = require("sync-request");
 
 var app = express();
 
 var girl = new Object();
-var token = "";
-var group = "twittonator";
 
-var countMembers = function(offset, max) {
-  var res = request("GET", "https://api.vk.com/method/groups.getMembers?group_id="
-    + group + "&offset=" + offset
-    + "&sort=id_desc&fields=sex,can_write_private_message,photo_400_orig,online,relation,city&v=5.60");
+var group = "pravda.show";
+var token;
+
+function quantityMembers() {
+  var res = request("GET", "https://api.vk.com/method/groups.getMembers?group_id=" + group + "&v=5.60");
   var body = JSON.parse(res.getBody());
-
-  if (body.response.count > max)
-    return max;
 
   return body.response.count;
 }
 
-var makeThousandGirls = function(offset) {
-  var res = request("GET", "https://api.vk.com/method/groups.getMembers?group_id="
-    + group + "&offset=" + offset
-    + "&sort=id_desc&fields=sex,can_write_private_message,photo_400_orig,online,relation,city&v=5.60");
-  var body = JSON.parse(res.getBody());
+function makeGirls(offset) {
+  var url = "https://api.vk.com/method/groups.getMembers?group_id=" + group + "&offset=" + offset + "&sort=id_desc&fields=sex,can_write_private_message,photo_max_orig,online,relation,city&v=5.60";
 
-  try {
-    var allID = body.response.items;
+  rp(url)
+    .then(function(body) {
+      var json = JSON.parse(body);
 
-    allID.forEach(function(profile) {
-      var firstName = profile.first_name;
-      var lastName = profile.last_name;
-      var fullName = firstName + " " + lastName;
+      var items = json.response.items;
+      var count = json.response.count;
 
-      var id = profile.id;
-      var sex = profile.sex;
+      items.forEach(function(profile) {
+        var id = profile.id;
+        var firstName = profile.first_name;
+        var lastName = profile.last_name;
+        var fullName = firstName + " " + lastName;
 
-      try {
-        var city = profile.city.id;
-        var relation = profile.relation;
-      } catch (e) {
-        // console.log("City or relation not found...");
-      }
+        try {
+          var city = profile.city.id;
+          var sex = profile.sex;
+          var photo = profile.photo_max_orig;
+          var online = profile.online;
+          var relation = profile.relation;
+          var message = profile.can_write_private_message;
 
-      var photo = profile.photo_400_orig;
-      var online = profile.online;
-      var relation = profile.relation;
-      var message = profile.can_write_private_message;
+          if (sex == 1 && city == 2 & message) {
+            // Message
+            var msg = encodeURIComponent("Привет, " + firstName + ".");
+            var sendMessageURL = "https://api.vk.com/method/messages.send?user_id=" + id + "&message=" + msg + "&access_token=" + token + "&v=5.60";
 
-      if (sex == 1 && city == 2 & message) {
-        var res = request("GET", "https://api.vk.com/method/users.getSubscriptions?&user_id=" + id + "&extended=1&count=200&v=5.60");
+            // Subscriptions
+            var groupsShit = "";
+            var groupsCount;
 
-        var body = JSON.parse(res.getBody());
-        var groupsCount = body.response.count;
-        var groupsAll = body.response.items;
-        var groupsAllList = new Array();
-        var groupsShitCount = 0;
+            rp("https://api.vk.com/method/users.getSubscriptions?&user_id=" + id + "&extended=1&count=200&v=5.60")
+              .then(function(body) {
+                var json = JSON.parse(body);
 
-        groupsAll.forEach(function(group) {
-          if (/Щебестан/i.test(group.name)
-              || /MDK/i.test(group.name)
-              || /FURGOS/i.test(group.name)
-              || /CUNBERS/i.test(group.name)
-              || /2ch/i.test(group.name)
-              || /Деградач/i.test(group.name)
-              || /Изолятор/i.test(group.name)
-              || /Паблик Долбоеба/i.test(group.name)
-              || /Овсянка, сэр/i.test(group.name)
-              || /FTP/i.test(group.name)
-              || /Ilita/i.test(group.name)
-              || /SLVK/i.test(group.name)
-              || /Amoral/i.test(group.name)
-              || /Webm/i.test(group.name)
-              || /мем/i.test(group.name)
-              || /mem/i.test(group.name)
-              || /chan/i.test(group.name)
-              || /4ch/i.test(group.name)) {
-            ++groupsShitCount;
+                var groupsAll = json.response.items;
+                    groupsCount = json.response.count;
+
+                groupsAll.forEach(function(group) {
+                  if (/Щебестан/i.test(group.name)
+                      || /MDK/i.test(group.name)
+                      || /FURGOS/i.test(group.name)
+                      || /CUNBERS/i.test(group.name)
+                      || /2ch/i.test(group.name)
+                      || /Деградач/i.test(group.name)
+                      || /Изолятор/i.test(group.name)
+                      || /Паблик Долбоеба/i.test(group.name)
+                      || /Овсянка, сэр/i.test(group.name)
+                      || /FTP/i.test(group.name)
+                      || /Ilita/i.test(group.name)
+                      || /SLVK/i.test(group.name)
+                      || /Amoral/i.test(group.name)
+                      || /Webm/i.test(group.name)
+                      || /мем/i.test(group.name)
+                      || /mem/i.test(group.name)
+                      || /chan/i.test(group.name)
+                      || /4ch/i.test(group.name)) {
+                    groupsShit += group.name + " ";
+                  }
+                });
+              })
+              .catch(function(err) {
+                // Error
+              });
+
+            // Wall
+            var posts = new Array();
+
+            rp("https://api.vk.com/method/wall.get?&owner_id=" + id + "&count=2&v=5.60")
+              .then(function(body) {
+                var json = JSON.parse(body);
+
+                var items = json.response.items;
+
+                items.forEach(function(wall) {
+                  var postLink = "https://vk.com/wall" + id + "_" + wall.id;
+
+                  posts.push(postLink);
+                });
+              })
+              .catch(function(err) {
+                // Error
+              });
+
+
+            setTimeout(function() {
+              girl[fullName] = new Object();
+              girl[fullName]["name"] = fullName;
+              girl[fullName]["id"] = id;
+              girl[fullName]["photo"] = photo;
+              girl[fullName]["relation"] = relation;
+              girl[fullName]["online"] = online;
+              girl[fullName]["groupsCount"] = groupsCount;
+              girl[fullName]["groupsShit"] = groupsShit;
+              girl[fullName]["posts"] = posts;
+              girl[fullName]["sendMessageURL"] = sendMessageURL;
+            }, 500);
           }
-        });
-
-        var msg = encodeURIComponent("Привет, " + firstName + ".");
-        var sendMessageURL = "https://api.vk.com/method/messages.send?user_id=" + id + "&message=" + msg + "&access_token=" + token + "&v=5.60";
-
-        girl[fullName] = new Object();
-        girl[fullName]["name"] = fullName;
-        girl[fullName]["id"] = id;
-        girl[fullName]["photo"] = photo;
-        girl[fullName]["relation"] = relation;
-        girl[fullName]["online"] = online;
-        girl[fullName]["groupsCount"] = groupsCount;
-        girl[fullName]["groupsShitCount"] = groupsShitCount;
-        // girl[fullName]["groupsAllList"] = groupsAllList;
-        girl[fullName]["sendMessageURL"] = sendMessageURL;
-      }
+        } catch (e) {
+          // console.log("Информации недостаточно.");
+        }
+      });
+    })
+    .catch(function(err) {
+      // Error
     });
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-app.use(express.static(__dirname + "/static"));
-app.set("view engine", "ejs");
-app.get('/', function(req, res) {
-  res.render("pages/index", {
-    data: girl
-  });  
-});
-
-for (var i = 0; i < countMembers(0, 40000); i += 1000) {
-  console.time("1000 загрузили.");
-
-  makeThousandGirls(i);
-
-  console.timeEnd("1000 загрузили.");
 }
 
-app.listen(8080);
+console.time("Полный парсинг участников");
 
-console.log("http://localhost:8080");
+// var count = quantityMembers();
+var count = 2000;
+
+for (var i = 0; i < count; i+= 1000) {
+  console.time("Тысяча участников");
+
+  makeGirls(i);
+
+  console.timeEnd("Тысяча участников");
+}
+
+console.timeEnd("Полный парсинг участников");
+
+setTimeout(function() {
+  app.use(express.static(__dirname + "/static"));
+  app.set("view engine", "ejs");
+  app.get('/', function(req, res) {
+    res.render("pages/index", {
+      data: girl
+    });
+  });
+
+  app.listen(8080);
+
+  console.log("http://localhost:8080");
+}, 500);
