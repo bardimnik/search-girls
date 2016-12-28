8debea55b2dd6a704d68702c471523677f5b5e90
Mikhail Semin <bifot@bifot.ru>
2016-12-29 00:53:20 +0300

    Added module blackgroups.  Added list of bad groups. Fixed package.

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-// var express = require("express");
-// var rp = require("request-promise");
-// var cheerio = require("cheerio");
-// var request = require("sync-request");
-
-// var app = express();
-
-// var girl = new Object();
 var express = require("express");
 var request = require("sync-request");
+var listOfShitGroups = require("./modules/blackgroups");
 
 var app = express();
 
 var girl = new Object();
-var token = "68c54403359b7bc2924e240ccb5c5417309613a307aff68970ce0e163a075cbb3ebdc8f790cc0a87f1100";
-var group = "pewtextposts";
+var token;
+var group = "naitivihod";
 
 function countMembers(offset, max) {
   var res = request("GET", "https://api.vk.com/method/groups.getMembers?group_id=" + group + "&offset=" + offset + "&v=5.60");
@@ -55,36 +48,26 @@ function makeThousandGirls(offset) {
       var message = profile.can_write_private_message;
 
       if (sex == 1 && city == 2 & message) {
-        // var res = request("GET", "https://api.vk.com/method/users.getSubscriptions?&user_id=" + id + "&extended=1&count=200&v=5.60");
+        var res = request("GET", "https://api.vk.com/method/users.getSubscriptions?&user_id=" + id + "&extended=1&count=200&v=5.60");
 
-        // var body = JSON.parse(res.getBody());
-        // var groupsCount = body.response.count;
-        // var groupsAll = body.response.items;
-        // var groupsAllList = new Array();
-        // var groupsShitCount = 0;
+        var body = JSON.parse(res.getBody());
+        var groupsCount = body.response.count;
+        var groupsAll = body.response.items;
+        var groupsShitList = "";
+        var groupsShitCount = 0;
 
-        // groupsAll.forEach(function(group) {
-        //   if (/Щебестан/i.test(group.name)
-        //       || /MDK/i.test(group.name)
-        //       || /FURGOS/i.test(group.name)
-        //       || /CUNBERS/i.test(group.name)
-        //       || /2ch/i.test(group.name)
-        //       || /Деградач/i.test(group.name)
-        //       || /Изолятор/i.test(group.name)
-        //       || /Паблик Долбоеба/i.test(group.name)
-        //       || /Овсянка, сэр/i.test(group.name)
-        //       || /FTP/i.test(group.name)
-        //       || /Ilita/i.test(group.name)
-        //       || /SLVK/i.test(group.name)
-        //       || /Amoral/i.test(group.name)
-        //       || /Webm/i.test(group.name)
-        //       || /мем/i.test(group.name)
-        //       || /mem/i.test(group.name)
-        //       || /chan/i.test(group.name)
-        //       || /4ch/i.test(group.name)) {
-        //     ++groupsShitCount;
-        //   }
-        // });
+        groupsAll.forEach(function(group) {
+          if (group.type == "page") {
+            for (blackGroup in listOfShitGroups) {
+              if (group.name.match(listOfShitGroups[blackGroup])) {
+                ++groupsShitCount;
+                groupsShitList += group.name + " ";
+              }
+            }
+          } else {
+            console.log("Пользователь подписан на страницу другого пользователя.");
+          }
+        });
 
         var msg = encodeURIComponent("Привет, " + firstName + ".");
         var sendMessageURL = "https://api.vk.com/method/messages.send?user_id=" + id + "&message=" + msg + "&access_token=" + token + "&v=5.60";
@@ -95,10 +78,10 @@ function makeThousandGirls(offset) {
         girl[fullName]["photo"] = photo;
         girl[fullName]["relation"] = relation;
         girl[fullName]["online"] = online;
-        // girl[fullName]["groupsCount"] = groupsCount;
-        // girl[fullName]["groupsShitCount"] = groupsShitCount;
-        // girl[fullName]["groupsAllList"] = groupsAllList;
         girl[fullName]["sendMessageURL"] = sendMessageURL;
+        girl[fullName]["groupsCount"] = groupsCount;
+        girl[fullName]["groupsShitCount"] = groupsShitCount;
+        girl[fullName]["groupsShitList"] = groupsShitList;
       }
     });
   } catch (e) {
---
+++ modules/blackgroups.js
@@ -0,0 +1,20 @@
+var listOfShitGroups = [
+  "найтивыход",
+  "Овсянка, сэр!",
+  "Щебестан",
+  "MDK",
+  "FURGOS",
+  "CUNBERS",
+  "2ch",
+  "Деградач",
+  "Изолятор",
+  "Паблик Долбоеба",
+  "FTP",
+  "Ilita!",
+  "SLVK",
+  "Amoral",
+  "WebM",
+  "4ch"
+];
+
+module.exports = listOfShitGroups;
