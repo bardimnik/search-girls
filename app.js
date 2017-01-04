0df457f4035691a48fa692906c0bf99ab2677d50
Mikhail Semin <bifot@bifot.ru>
2017-01-05 02:12:22 +0300

    Асинхронный парсинг, правильный запуск сервера.

--- app.js
+++ app.js
@@ -1,112 +1,146 @@
 var express = require("express");
 var request = require("sync-request");
+var rp = require("request-promise");
 var listOfShitGroups = require("./modules/blackgroups");
 
 var app = express();
 
 var girl = new Object();
-var token;
-var group = "naitivihod";
+var token = "token";
+var group = "pravda.show";
 
-function countMembers(offset, max) {
+function getCountMembers(group, offset, max) {
   var res = request("GET", "https://api.vk.com/method/groups.getMembers?group_id=" + group + "&offset=" + offset + "&v=5.60");
   var body = JSON.parse(res.getBody());
 
-  if (body.response.count > max) 
-    return max;
+  var count = (body.response.count > max) ? max : body.response.count;
 
-  return body.response.count;
+  return count;
 }
 
-function makeThousandGirls(offset) {
-  var res = request("GET", "https://api.vk.com/method/groups.getMembers?group_id="
+function makeThousandGirls(group, offset) {
+  var options = {
+    uri: "https://api.vk.com/method/groups.getMembers?group_id="
     + group + "&offset=" + offset
-    + "&sort=id_desc&fields=sex,can_write_private_message,photo_max_orig,online,relation,city&v=5.60");
-  var body = JSON.parse(res.getBody());
+    + "&sort=id_desc&fields=sex,can_write_private_message,photo_max_orig,online,relation,city&v=5.60",
+    json: true
+  };
 
-  try {
-    var profiles = body.response.items;
+  rp(options)
+    .then(body => {
+      var profiles = body.response.items;
 
-    profiles.forEach(function(profile) {
-      var name = profile.first_name + " " + profile.last_name;
+      profiles.forEach(function(profile) {
+        var firstName = profile.first_name;
+        var lastName = profile.last_name;
+        var fullName = firstName + " " + lastName;
 
-      var id = profile.id;
-      var sex = profile.sex;
+        var id = profile.id;
+        var sex = profile.sex;
 
-      try {
-        var city = profile.city.id;
+        try {
+          var city = profile.city.id;
+          var relation = profile.relation;
+        } catch (e) {
+          // console.log("City or relation not found...");
+        }
+
+        var photo = profile.photo_max_orig;
+        var online = profile.online;
         var relation = profile.relation;
-      } catch (e) {
-        // console.log("City or relation not found...");
-      }
+        var message = profile.can_write_private_message;
 
-      var photo = profile.photo_max_orig;
-      var online = profile.online;
-      var relation = profile.relation;
-      var message = profile.can_write_private_message;
+        if (sex == 1 && city == 2 & message) {
+          /*
 
-      if (sex == 1 && city == 2 & message) {
-        var res = request("GET", "https://api.vk.com/method/users.getSubscriptions?&user_id=" + id + "&extended=1&count=200&v=5.60");
+          *******
 
-        var body = JSON.parse(res.getBody());
-        var groupsCount = body.response.count;
-        var groupsAll = body.response.items;
-        var groupsShitList = "";
-        var groupsShitCount = 0;
+          Делая запросы к подпискам пользователя, парсинг очень сильно замедляется,
+          поэтому не рекомендую это делать.
 
-        groupsAll.forEach(function(group) {
-          if (group.type == "page") {
-            for (blackGroup in listOfShitGroups) {
-              if (group.name.match(listOfShitGroups[blackGroup])) {
-                ++groupsShitCount;
-                groupsShitList += group.name + " ";
+          *******
+
+          var res = request("GET", "https://api.vk.com/method/users.getSubscriptions?&user_id=" + id + "&extended=1&count=200&v=5.60");
+
+          var body = JSON.parse(res.getBody());
+          var groupsCount = body.response.count;
+          var groupsAll = body.response.items;
+          var groupsShitList = "";
+          var groupsShitCount = 0;
+
+          groupsAll.forEach(function(group) {
+            if (group.type == "page") {
+              for (blackGroup in listOfShitGroups) {
+                if (group.name.match(listOfShitGroups[blackGroup])) {
+                  ++groupsShitCount;
+                  groupsShitList += group.name + " ";
+                }
               }
+            } else {
+              console.log("Пользователь подписан на страницу другого пользователя.");
             }
-          } else {
-            console.log("Пользователь подписан на страницу другого пользователя.");
-          }
-        });
+          });
 
-        var msg = encodeURIComponent("Привет, " + firstName + ". Не против познакомиться?");
-        var sendMessageURL = "https://api.vk.com/method/messages.send?user_id=" + id + "&message=" + msg + "&access_token=" + token + "&v=5.60";
+          */
 
-        girl[name] = new Object();
-        girl[name]["name"] = name;
-        girl[name]["id"] = id;
-        girl[name]["photo"] = photo;
-        girl[name]["relation"] = relation;
-        girl[name]["online"] = online;
-        girl[name]["sendMessageURL"] = sendMessageURL;
-        girl[name]["groupsCount"] = groupsCount;
-        girl[name]["groupsShitCount"] = groupsShitCount;
-        girl[name]["groupsShitList"] = groupsShitList;
-      }
+          var msg = encodeURIComponent("Привет, " + firstName + ".");
+          var sendMessageURL = "https://api.vk.com/method/messages.send?user_id=" + id + "&message=" + msg + "&access_token=" + token + "&v=5.60";
+
+          girl[fullName] = new Object();
+          girl[fullName]["name"] = fullName;
+          girl[fullName]["id"] = id;
+          girl[fullName]["photo"] = photo;
+          girl[fullName]["relation"] = relation;
+          girl[fullName]["online"] = online;
+          girl[fullName]["sendMessageURL"] = sendMessageURL;
+
+          /*
+
+          girl[fullName]["groupsCount"] = groupsCount;
+          girl[fullName]["groupsShitCount"] = groupsShitCount;
+          girl[fullName]["groupsShitList"] = groupsShitList;
+
+          */
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  } catch (e) {
-    console.log(e);
-  }
 };
 
-app.use(express.static(__dirname + "/static"));
-app.set("view engine", "ejs");
-app.get('/', function(req, res) {
-  res.render("pages/index", {
-    data: girl
-  });  
+var server = new Promise((resolve, reject) => {
+  var count = getCountMembers(group, 0, 150000);
+
+  for (var i = 0; i <= count; i += 1000) {
+    makeThousandGirls(group, i);
+
+    if (i == count) {
+      resolve(i);
+
+      return;
+    }
+  }
+    
+  reject("Error");
 });
 
-console.time("Полный парсинг участников");
+server
+  .then(
+    (result) => {
+      app.use(express.static(__dirname + "/static"));
+      app.set("view engine", "ejs");
+      app.get('/', function(req, res) {
+        res.render("pages/index", {
+          data: girl
+        });  
+      });
 
-for (var i = 0, count = countMembers(0, 50000); i < count; i += 1000) {
-  console.time("1000 загрузили.");
+      app.listen(1488);
 
-  makeThousandGirls(i);
-
-  console.timeEnd("1000 загрузили.");
-}
-
-console.timeEnd("Полный парсинг участников");
-
-app.listen(1337);
-
-console.log("http://localhost:1337");
+      console.log("http://localhost:1488");
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
