392267e1cdf3a61261e47360a7156eb9693fba26
Mikhail Semin <bifot@bifot.ru>
2017-01-16 23:21:05 +0300

    Минимизация CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
 
 // Compile Our Sass
 gulp.task('sass', function() {
   return gulp.src('static/scss/*.scss')
     .pipe(sass())
+    .pipe(cssnano())
     .pipe(gulp.dest('static/css'));
 });
 
