78f3abd5eb77064ceac214f7c412068e9c78a871
Mikhail Semin <bifot@bifot.ru>
2017-02-23 00:00:24 +0300

    Рефакторинг, стильное меню.

--- static/js/common.js
+++ static/js/common.js
@@ -1,118 +1,78 @@
-var container = document.querySelector('.container');
-var offlineItems = document.querySelectorAll('.girls__item--offline');
-var girls = document.querySelector('.girls');
-var girlsItem = document.querySelectorAll('.girls__item');
-var girlsItemShit = document.querySelectorAll('.girls__item--shit');
-var girlsPhoto = document.querySelectorAll('.girls__photo');
-var girlsGroups = document.querySelectorAll('.girls__groups');
-var girlsShowShit = document.querySelectorAll('.girls__show-shit');
+var container = $('.container');
 
-var viewNumber = document.querySelector('.view__number');
-var viewWidth = document.querySelector('.view__width');
-var viewColumnFour = document.querySelector('.view__column--four');
-var viewColumnEight = document.querySelector('.view__column--eight');
-var viewOnline = document.querySelector('.view__online');
-var viewShit = document.querySelector('.view__shit');
+var offlineItems = $('.girls__item--offline');
+var girls = $('.girls');
+var girlsItem = $('.girls__item');
+var girlsItemShit = $('.girls__item--shit');
+var girlsPhoto = $('.girls__photo');
+var girlsGroups = $('.girls__groups');
+var girlsShowShit = $('.girls__show-shit');
+
+var view = $('.view');
+var viewNumber = $('.view__number');
+var viewColumnFour = $('.view__column--four');
+var viewColumnEight = $('.view__column--eight');
+var viewOnline = $('.view__online');
+var viewShit = $('.view__shit');
 
 var findCountGirls = () => {
   var count = 0;
 
-  girlsItem.forEach(item => {
-    if (item.style.display != 'none') count ++;
+  girlsItem.each((i, item) => {
+    if ($(item).css('display') !== 'none') count++;
   });
 
-  viewNumber.innerHTML = count;  
+  viewNumber.html(count);
 };
 
-var changeColumns = (checkbox, columns, anotherColumns) => {
-  if (checkbox.checked) {
-    // Снимаем один чекбокс
-    if (checkbox == viewColumnFour) {
-      viewColumnEight.checked = false;
-    } else if (checkbox == viewColumnEight) {
-      viewColumnFour.checked = false;
-    }
+var changeColumns = (link, columns) => {
+  if (!link.hasClass('active')) {
+    // Деактивируем одну ссылку
+    (link == viewColumnFour) ? viewColumnEight.removeClass('active'): viewColumnFour.removeClass('active');
 
-    var containerWidth = container.getBoundingClientRect().width;
-    var itemWidth = containerWidth / columns;
+    var itemWidth = container.innerWidth() / columns;
 
-    Object.assign(container.style, {
-      columnCount: columns,
-      columnGap: '10px'
-    });
+    container.css('columnCount', columns);
 
-    girlsItem.forEach(block => {
-      block.style.width = `${itemWidth}px`;
-    });
+    girlsItem.each((i, item) => $(item).css('width', `${itemWidth}px`));
+
+    link.addClass('active');
   } else {
-    Object.assign(container.style, {
-      columnCount: 6,
-      columnGap: '5px'
-    });
+    container.css('columnCount', 6);
 
-    girlsItem.forEach(block => {
-      block.style.width = 'auto';
-    });
+    girlsItem.each((i, item) => $(item).css('width', 'auto'));
+
+    link.removeClass('active');
   }
 };
-
-$(viewWidth).on('click', () => {
-  if (!viewWidth.checked) {
-    Object.assign(container.style, {
-      maxWidth: '1170px',
-      width: '100%'
-    });
-
-    // Если количество колонок изменено, а затем что-то происходит с шириной,
-    // то запрашиваем еще раз изменение колонок, чтобы перерассчитать ширину.
-    if (viewColumnFour.checked) {
-      changeColumns(viewColumnFour, 4, 2);
-    } else if (viewColumnEight.checked) {
-      changeColumns(viewColumnEight, 8, 2);
-    }
-  } else {
-    Object.assign(container.style, {
-      maxWidth: 'none',
-      columnGap: '5px'
-    });
-
-    girlsItem.forEach(block => {
-      block.style.width = 'auto';
-    });
-  }
-});
 
 $(viewColumnFour).on('click', () => {
-  changeColumns(viewColumnFour, 4, 2);
+  changeColumns(viewColumnFour, 4);
 });
 
 $(viewColumnEight).on('click', () => {
-  changeColumns(viewColumnEight, 8, 2);
+  changeColumns(viewColumnEight, 8);
 });
 
 $(viewOnline).on('click', () => {
-  if (viewOnline.checked) {
-    offlineItems.forEach(block => {
-      block.style.display = 'none';
-    });
+  if (!viewOnline.hasClass('active')) {
+    offlineItems.each((i, item) => $(item).css('display', 'none'));
+    viewOnline.addClass('active');
   } else {
-    offlineItems.forEach(block => {
-      block.style.display = 'block';
-    });
+    offlineItems.each((i, item) => $(item).css('display', 'block'));
+    viewOnline.removeClass('active');
   }
 
   findCountGirls();
 });
 
 $(viewShit).on('click', () => {
-  if (viewShit.checked) {
-    girlsItemShit.forEach(item => {
-      item.style.display = 'none';
-    });
+  if (!viewShit.hasClass('active')) {
+    girlsItemShit.each((i, item) => $(item).css('display', 'none'));
+    viewShit.addClass('active');
   } else {
-    girlsItemShit.forEach(item => {
-      item.style.display = 'block';
-    });
+    girlsItemShit.each((i, item) => $(item).css('display', 'block'));
+    viewShit.removeClass('active');
   }
 
   findCountGirls();
@@ -121,18 +81,18 @@ $(viewShit).on('click', () => {
 $(girlsGroups).on('click', event => {
   var target = event.target;
 
-  if (target.className != 'girls__show-shit') return;
+  if (target.className !== 'girls__show-shit') return;
 
   var item = target.parentNode;
   var link = item.childNodes[3];
   var list = item.childNodes[5];
 
-  link.style.display = 'block';
-  list.style.display = 'block';
+  $(link).css('display', 'block');
+  $(list).css('display', 'block');
 
   $(link).on('click', () => {
-    link.style.display = 'none';
-    list.style.display = 'none';
+    $(link).css('display', 'none');
+    $(list).css('display', 'none');
   });
 });
 
@@ -140,7 +100,7 @@ var nw = url => {
   var options = "menubar=no,location=no,resizable=no,scrollbars=no,status=no,width=200,height=200,left=500,top=500";
   var wind = window.open(url, 'message', options);
 
-  setTimeout(() => wind.close(), 150);
+  setTimeout(() => wind.close(), 750);
 };
 
 findCountGirls();
